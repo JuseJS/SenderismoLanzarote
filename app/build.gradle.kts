e89b44dcfa55ee386c3fa9f5384e0a6faedbeaef
Navarro <joseanp0202@gmail.com>
2024-12-12 13:15:12 +0000

    chore(dependencies): update project dependencies
    - Updated Kotlin version to 2.1.0 and AGP version to 8.7.3.
    - Added dependencies for OSMDroid and mapsforge.
    - Organized dependencies in `build.gradle.kts` and `libs.versions.toml`.
    - Included new permissions in `AndroidManifest.xml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,33 +27,48 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-
+    // AndroidX Core
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+
+    // Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+
+    // Maps
+    implementation(libs.osmdroid)
+    implementation(libs.osmdroid.mapsforge)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    // Debug
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
