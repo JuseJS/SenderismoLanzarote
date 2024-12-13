1ebd487a74cd4ea4a0cbf48f8eb232f527f98d0c
Navarro <joseanp0202@gmail.com>
2024-12-13 13:30:00 +0000

    feat(reference-dao): add SeasonRouteDao
    - Implemented DAO for `SeasonRoute` with methods to fetch all seasons and fetch a season by ID.

---
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/dao/reference/SeasonRouteDao.kt
@@ -0,0 +1,14 @@
+package org.iesharia.senderismolanzarote.data.database.dao.reference
+
+import androidx.room.*
+import org.iesharia.senderismolanzarote.data.database.entity.route.reference.SeasonRoute
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SeasonRouteDao {
+    @Query("SELECT * FROM season_routes")
+    fun getAllSeasonRoutes(): Flow<List<SeasonRoute>>
+
+    @Query("SELECT * FROM season_routes WHERE id = :id")
+    suspend fun getSeasonRouteById(id: Int): SeasonRoute?
+}
