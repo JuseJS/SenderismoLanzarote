c35c6cf4964d7a5a07d4203af1ee5297e85d7c0d
Navarro <joseanp0202@gmail.com>
2024-12-13 13:29:04 +0000

    feat(reference-dao): add DifficultyLevelDao
    - Implemented DAO for `DifficultyLevel` with methods to fetch all levels and fetch a level by ID.

---
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/dao/reference/DifficultyLevelDao.kt
@@ -0,0 +1,14 @@
+package org.iesharia.senderismolanzarote.data.database.dao.reference
+
+import androidx.room.*
+import org.iesharia.senderismolanzarote.data.database.entity.route.reference.DifficultyLevel
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DifficultyLevelDao {
+    @Query("SELECT * FROM difficulty_levels")
+    fun getAllDifficultyLevels(): Flow<List<DifficultyLevel>>
+
+    @Query("SELECT * FROM difficulty_levels WHERE id = :id")
+    suspend fun getDifficultyLevelById(id: Int): DifficultyLevel?
+}
