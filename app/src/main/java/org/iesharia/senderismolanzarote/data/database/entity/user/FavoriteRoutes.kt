7745485f67fecdec34ef7b89b3392e46afa4055f
Navarro <joseanp0202@gmail.com>
2024-12-13 13:18:54 +0000

    feat(users): add FavoriteRoutes entity
    - Created the `FavoriteRoutes` entity with relationships to `User` and `Route`.
    - Added fields for user-route mapping and added date.

--- app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/user/FavoriteRoutes.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/user/FavoriteRoutes.kt
@@ -1,4 +1,25 @@
 package org.iesharia.senderismolanzarote.data.database.entity.user
 
-class FavoriteRoutes {
-}
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "favorite_routes",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["id"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("userId")],
+    primaryKeys = ["userId", "routeId"]
+)
+data class FavoriteRoute(
+    val userId: Int,
+    val routeId: Int,
+    val addedDate: LocalDateTime
+)
