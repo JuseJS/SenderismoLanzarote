75a8efee914a1468bdf8ca4e9271bd6e5c82f6f6
Navarro <joseanp0202@gmail.com>
2024-12-13 13:16:25 +0000

    feat(routes): add ActivityRecord entity
    - Created the `ActivityRecord` entity with relationships to `User` and `Route`.
    - Added fields for tracking user activity, including distance, time, and comments.

--- app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/route/main/ActivityRecord.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/route/main/ActivityRecord.kt
@@ -1,4 +1,43 @@
 package org.iesharia.senderismolanzarote.data.database.entity.route.main
 
-class ActivityRecord {
-}
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import org.iesharia.senderismolanzarote.data.database.entity.user.User
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+@Entity(
+    tableName = "activity_records",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["id"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Route::class,
+            parentColumns = ["id"],
+            childColumns = ["routeId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [
+        Index("userId"),
+        Index("routeId")
+    ]
+)
+data class ActivityRecord(
+    @PrimaryKey
+    val id: Int,
+    val userId: Int,
+    val routeId: Int,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+    val totalTime: LocalTime,
+    val actualDistanceKm: BigDecimal,
+    val comments: String
+)
