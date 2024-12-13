1bb56a54374c13d249cbcf056cb99e3993e5178e
Navarro <joseanp0202@gmail.com>
2024-12-13 13:18:05 +0000

    feat(route-references): add PoiType entity
    - Created the `PoiType` entity with fields for type and description.

--- app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/route/reference/PoiType.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/route/reference/PoiType.kt
@@ -1,4 +1,12 @@
 package org.iesharia.senderismolanzarote.data.database.entity.route.reference
 
-class PoiType {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "poi_types")
+data class PoiType(
+    @PrimaryKey
+    val id: Int,
+    val type: String,
+    val description: String
+)
