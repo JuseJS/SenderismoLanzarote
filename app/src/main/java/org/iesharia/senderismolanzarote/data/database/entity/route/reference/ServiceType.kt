70e656134e3110f75beeb32d3f66d0ec8ed3485a
Navarro <joseanp0202@gmail.com>
2024-12-13 13:18:48 +0000

    feat(route-references): add ServiceType entity
    - Created the `ServiceType` entity with fields for type and description.

--- app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/route/reference/ServiceType.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/route/reference/ServiceType.kt
@@ -1,4 +1,12 @@
 package org.iesharia.senderismolanzarote.data.database.entity.route.reference
 
-class ServiceType {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "service_types")
+data class ServiceType(
+    @PrimaryKey
+    val id: Int,
+    val type: String,
+    val description: String
+)
