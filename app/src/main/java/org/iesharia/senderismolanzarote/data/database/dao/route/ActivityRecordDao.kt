ad31e4b508b007a9932772f20530826a1a674404
Navarro <joseanp0202@gmail.com>
2024-12-13 13:30:16 +0000

    feat(route-dao): add ActivityRecordDao
    - Implemented DAO for `ActivityRecord` with methods to fetch activity records by user or route.
    - Included methods for insert, update, and delete operations.

---
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/dao/route/ActivityRecordDao.kt
@@ -0,0 +1,23 @@
+package org.iesharia.senderismolanzarote.data.database.dao.route
+
+import androidx.room.*
+import org.iesharia.senderismolanzarote.data.database.entity.route.main.ActivityRecord
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ActivityRecordDao {
+    @Query("SELECT * FROM activity_records WHERE userId = :userId")
+    fun getUserActivityRecords(userId: Int): Flow<List<ActivityRecord>>
+
+    @Query("SELECT * FROM activity_records WHERE routeId = :routeId")
+    fun getRouteActivityRecords(routeId: Int): Flow<List<ActivityRecord>>
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertActivityRecord(activityRecord: ActivityRecord)
+
+    @Update
+    suspend fun updateActivityRecord(activityRecord: ActivityRecord)
+
+    @Delete
+    suspend fun deleteActivityRecord(activityRecord: ActivityRecord)
+}
