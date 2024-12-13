05cb54a3d75637e29178bb5982e7e3c14d3edad0
Navarro <joseanp0202@gmail.com>
2024-12-13 13:18:16 +0000

    feat(route-references): add RouteStatus entity
    - Created the `RouteStatus` entity with fields for status and description.

--- app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/route/reference/RouteStatus.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/route/reference/RouteStatus.kt
@@ -1,4 +1,12 @@
 package org.iesharia.senderismolanzarote.data.database.entity.route.reference
 
-class RouteStatus {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "route_statuses")
+data class RouteStatus(
+    @PrimaryKey
+    val id: Int,
+    val status: String,
+    val description: String
+)
