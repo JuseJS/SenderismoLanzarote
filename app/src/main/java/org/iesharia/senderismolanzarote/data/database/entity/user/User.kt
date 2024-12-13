fd1c3eea7e3a82293566ea67188cad3d5b18bc5b
Navarro <joseanp0202@gmail.com>
2024-12-13 13:19:00 +0000

    feat(users): add User entity
    - Created the `User` entity with relationships to `UserRole`.
    - Defined fields for user details, including username, email, and profile image.

--- app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/user/User.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/database/entity/user/User.kt
@@ -1,4 +1,32 @@
 package org.iesharia.senderismolanzarote.data.database.entity.user
 
-class User {
-}
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "users",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserRole::class,
+            parentColumns = ["id"],
+            childColumns = ["rolId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("rolId")]
+)
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val username: String,
+    val email: String,
+    val passwordHash: String,
+    val firstName: String,
+    val lastName: String,
+    val registrationDate: LocalDateTime,
+    val rolId: Int,
+    val profileImage: String?
+)
