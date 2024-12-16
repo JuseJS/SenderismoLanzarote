af3d763e8eddf3e8d4ab05d9a89f600a199fee03
Navarro <joseanp0202@gmail.com>
2024-12-16 18:25:30 +0000

    Add ErrorLogger.\n\nSummary:\nIntroduced ErrorLogger to centralize logging for application errors.

--- app/src/main/java/org/iesharia/senderismolanzarote/data/logger/ErrorLogger.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/data/logger/ErrorLogger.kt
@@ -1,4 +1,14 @@
 package org.iesharia.senderismolanzarote.data.logger
 
-class ErrorLogger {
+import android.util.Log
+import org.iesharia.senderismolanzarote.domain.model.error.ErrorModel
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ErrorLogger @Inject constructor() {
+    fun logError(error: ErrorModel) {
+        val tag = "SenderismoApp"
+        Log.e(tag, "${error.type}: ${error.message}", error.cause)
+    }
 }
