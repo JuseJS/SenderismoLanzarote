2003e5a088240bfc6c78a491a4a154536a375e97
Navarro <joseanp0202@gmail.com>
2024-12-16 18:25:30 +0000

    Add ErrorModule.\n\nSummary:\nDefined ErrorModule for dependency injection of error-related components.

--- app/src/main/java/org/iesharia/senderismolanzarote/di/ErrorModule.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/di/ErrorModule.kt
@@ -1,4 +1,21 @@
 package org.iesharia.senderismolanzarote.di
 
-class ErrorModule {
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.iesharia.senderismolanzarote.data.handler.ErrorHandler
+import org.iesharia.senderismolanzarote.data.logger.ErrorLogger
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ErrorModule {
+    @Provides
+    @Singleton
+    fun provideErrorHandler(): ErrorHandler = ErrorHandler()
+
+    @Provides
+    @Singleton
+    fun provideErrorLogger(): ErrorLogger = ErrorLogger()
 }
