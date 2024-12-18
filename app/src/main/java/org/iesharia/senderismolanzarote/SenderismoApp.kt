c921f8c2cda6f535ee99e8727e4ce1a445cded51
Navarro <joseanp0202@gmail.com>
2024-12-18 06:20:18 +0000

    feat: Update SenderismoApp with navigation setup and initialization

--- app/src/main/java/org/iesharia/senderismolanzarote/SenderismoApp.kt
+++ app/src/main/java/org/iesharia/senderismolanzarote/SenderismoApp.kt
@@ -2,6 +2,17 @@ package org.iesharia.senderismolanzarote
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.osmdroid.config.Configuration
+import java.io.File
 
 @HiltAndroidApp
-class SenderismoApp : Application()
+class SenderismoApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Configuration.getInstance().let { config ->
+            config.userAgentValue = packageName
+            config.osmdroidBasePath = File(cacheDir, "osmdroid")
+            config.osmdroidTileCache = File(config.osmdroidBasePath, "tiles")
+        }
+    }
+}
